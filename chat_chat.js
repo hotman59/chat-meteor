b3aa90ee69b5b8e339bd46adc2a66fea957f61bb
othman <benrahmaneothman@gmail.com>
2016-02-26 20:41:10 +0100

    login done!

--- chat_chat.js
+++ chat_chat.js
@@ -4,26 +4,30 @@ if (Meteor.isClient) {
   //Session.setDefault('counter', 0);
 
   Template.messages.helpers({
-    messages: function () {
+    messages: function() {
       return Messages.find({});
     }
   });
 
 
-Template.messages.events({
-  'keypress textarea': function(e, instance){
-    if (e.keyCode == 13){
-      var value = instance.find('textarea').value;
-      instance.find('textarea').value='';
+  Template.messages.events({
+    'keypress textarea': function(e, instance) {
+      if (e.keyCode == 13) {
+        var value = instance.find('textarea').value;
+        instance.find('textarea').value = '';
 
         Messages.insert({
           message: value,
           timestamp: new Date()
-          //user.Meteor.userId()
+            //user.Meteor.userId()
         });
+      }
     }
-  }
-})
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
+  });
 }
 /*
   Template.hello.events({
@@ -35,7 +39,7 @@ Template.messages.events({
 
 */
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
   });
 }
