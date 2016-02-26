f2dd78cc5e98c4b5d47fee716422d6c179087bec
othman <benrahmaneothman@gmail.com>
2016-02-26 19:33:20 +0100

    begin meessenger chat

---
+++ chat_chat.js
@@ -0,0 +1,41 @@
+Messages = new Mongo.Collection('messages');
+if (Meteor.isClient) {
+  // counter starts at 0
+  //Session.setDefault('counter', 0);
+
+  Template.messages.helpers({
+    messages: function () {
+      return Messages.find({});
+    }
+  });
+
+
+Template.messages.events({
+  'keypress textarea': function(e, instance){
+    if (e.keyCode == 13){
+      var value = instance.find('textarea').value;
+      instance.find('textarea').value='';
+
+        Messages.insert({
+          message: value,
+          timestamp: new Date()
+          //user.Meteor.userId()
+        });
+    }
+  }
+})
+}
+/*
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+
+*/
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
